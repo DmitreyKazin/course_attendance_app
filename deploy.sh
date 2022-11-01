dd307a721a6d888b0a032751cf3c36b99643ed45
DmitreyKazin <dmitreyka@mta.ac.il>
2022-11-01 15:00:32 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,16 @@ cd /home/ec2-user/.ssh
 scp -i "app-production.pem" /home/ec2-user/workspace/release-pipeline/docker-compose-prod.yaml ec2-user@ex2-35-78-75-153.ap-northeast-1.compute.amazonaws.com:/home/ec2-user/
 
 # login with ssh to ec2-user on production server
-ssh -i "app-production.pem" ec2-user@ec2-35-78-75-153.ap-northeast-1.compute.amazonaws.com
+#ssh -i "app-production.pem" ec2-user@ec2-35-78-75-153.ap-northeast-1.compute.amazonaws.com
 
 # delete existing containers & images
-docker ps -aq | xargs docker rm -f
-docker images -q | xargs docker rmi -f
+#docker ps -aq | xargs docker rm -f
+#docker images -q | xargs docker rmi -f
 
 # bring the application up
-cd /home/ec2-user
-docker-compose -f docker-compose-prod.yaml -d --build
+#cd /home/ec2-user
+#docker-compose -f docker-compose-prod.yaml -d --build
 
-echo "DEPLOY TO PRODCUTION: SUCCESS"
+#echo "DEPLOY TO PRODCUTION: SUCCESS"
 
 
