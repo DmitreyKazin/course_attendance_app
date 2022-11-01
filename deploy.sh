db0966e7bf73b96c0302e2d807b2e7664ec2aec8
DmitreyKazin <dmitreyka@mta.ac.il>
2022-11-01 14:39:30 +0200

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,21 @@
 
 echo "DEPLOY TO PRODUCTION: STARTED"
 
-# ssh login to ec2-user in production server
+# copying docker-compose-prod.yaml to production server
 cd /home/ec2-user/.ssh
+scp -i "app-production.pem" /home/ec2-user/workspace/release-pipeline/docker-compose-prod.yaml ec2-user@ex2-35-78-75-153.ap-northeast-1.compute.amazonaws.com:/home/ec2-user/
 
+# login with ssh to ec2-user on production server
 ssh -i "app-production.pem" ec2-user@ec2-35-78-75-153.ap-northeast-1.compute.amazonaws.com
 
-# deleting existing containers & images
+# delete existing containers & images
 docker ps -aq | xargs docker rm -f
 docker images -q | xargs docker rmi -f
 
+# bring the application up
+cd /home/ec2-user
+docker-compose -f docker-compose-prod.yaml -d --build
+
+echo "DEPLOY TO PRODCUTION: SUCCESS"
 
 
