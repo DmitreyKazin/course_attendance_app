66a82e21d832ee46c8777f194ac7fb315a1981c5
DmitreyKazin <dmitreyka@mta.ac.il>
2022-11-01 15:08:05 +0200

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "DEPLOY TO PRODUCTION: STARTED"
 
 # copying docker-compose-prod.yaml to production server
 cd /home/ec2-user/.ssh
-scp -i "id_rsa" /home/ec2-user/workspace/release-pipeline/docker-compose-prod.yaml ec2-user@ex2-35-78-75-153.ap-northeast-1.compute.amazonaws.com:/home/ec2-user/
+scp -i "id_rsa" /home/ec2-user/workspace/release-pipeline/docker-compose-prod.yaml ec2-user@ec2-35-78-75-153.ap-northeast-1.compute.amazonaws.com:/home/ec2-user/
 
 # login with ssh to ec2-user on production server
 #ssh -i "app-production.pem" ec2-user@ec2-35-78-75-153.ap-northeast-1.compute.amazonaws.com
