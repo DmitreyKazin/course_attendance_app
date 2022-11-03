8bfe94d713675c6dcbc7778cd44209f9945a07ac
DmitreyKazin <dmitreyka@mta.ac.il>
2022-11-03 22:35:37 +0200

    removed stage

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,6 @@ ec2-user@172.31.7.202 \
 << EOF
 	cd /home/ec2-user/
 	docker-compose -f docker-compose-prod.yaml down
-	docker ps -aq | xargs docker rm -f
 	docker images -q | xargs docker rmi -f
 	docker-compose -f docker-compose-prod.yaml up -d --build
 EOF
