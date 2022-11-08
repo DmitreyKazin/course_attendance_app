f30237bae351496bf4f984086ab651081b06cea1
DmitreyKazin <dmitreyka@mta.ac.il>
2022-11-08 18:16:52 +0200

    typo

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,16 @@
 STAGE=${1}
 PROD_IP=172.31.7.202
 STAGING_IP=172.31.47.116
-DEPLOY_ENV=""
+DEPLOY_ENV=''
 
 if [ $STAGE=="production" ];
 then
 	DEPLOY_ENV=$PROD_IP
+	echo $DEPLOY_ENV
 elif [ $STAGE=="staging" ];
 then
-	DPELOY_ENV=$STAGING_IP
+	DEPLOY_ENV=$STAGING_IP
+	echo $DEPLOY_ENV
 else
 	echo "ERROR!!!\nONLY 'production' OR 'test' ARE ACCEPTED"
 	exit 1
