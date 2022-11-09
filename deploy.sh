df06fb5893f6c116391595abd6285e58c682ffc2
Dmitrey Kazin <kazindmitrey@gmail.com>
2022-11-09 19:35:47 +0200

    added sleep

--- deploy.sh
+++ deploy.sh
@@ -36,11 +36,12 @@ cd /home/ec2-user/
 docker-compose -f docker-compose-prod.yml down
 docker images -q | xargs docker rmi -f
 docker-compose -f docker-compose-prod.yml up -d --no-build
+sleep 15
 HTTPS_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" https://127.0.0.1:5000/)
 if [ $HTTP_STATUS -eq 200 ];
 then
 	echo "SUCCESS"
 else
-	echo "SUCCESS"
+	echo "FAIL"
 fi
 EOF
